bd436da8b263c460f773f6b47aaab17c473154ec
Maksim Ruzhytskiy <maksimkaya33@gmail.com>
2025-02-04 10:22:24 +0200

    build the app

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/drova-dubovi/",
-  plugins: [react()],
-  root: ".", // Корінь проєкту
-});
